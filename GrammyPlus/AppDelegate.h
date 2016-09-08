2d5b1480998b373f97168d26270ba4d619d58ea7
David Szemenkar <dszemenkar@apple.com>
2016-09-08 14:20:58 +0200

    Initial Commit

---
+++ GrammyPlus/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GrammyPlus
+//
+//  Created by David Szemenkar on 2016-09-08.
+//  Copyright © 2016 David Szemenkar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GrammyPlus/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GrammyPlus
+//
+//  Created by David Szemenkar on 2016-09-08.
+//  Copyright © 2016 David Szemenkar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
